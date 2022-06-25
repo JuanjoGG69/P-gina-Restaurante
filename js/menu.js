ac58641f3fbe94816de395fc0bc022d309ebbdb6
Juan Jose Martinez Aguila <jjmartagui@gmail.com>
2022-06-25 22:16:28 +0200

    Hecho el menu de navegacion del index.html

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+const btnMenu = document.querySelector('#btnmenu');
+const menu = document.querySelector('#menu');
+
+btnMenu.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+    
+})
+
+
